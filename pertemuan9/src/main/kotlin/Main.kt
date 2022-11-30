44cc52998130808e1f1f73cf43940cb40d719e76
Pranarendra Dwikurnia <92442022+Pranarendra08@users.noreply.github.com>
2022-11-30 21:46:16 +0700

    Add files via upload

---
+++ pertemuan9/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+//    launch() {
+//        delay(1000L)
+//        println("Tungguin woyy!!")
+//    }
+//    println("Cabs duls")
+    val timeOne = measureTimeMillis {
+        val capital = getCapital()
+        val income = getIncome()
+        println("Your profit is ${income - capital}")
+    }
+
+    val timeTwo = measureTimeMillis {
+        val capital = async { getCapital() }
+        val income = async { getIncome() }
+        println("Your profit is ${income.await() - capital.await()}")
+    }
+
+    println("Completed in $timeOne ms vs $timeTwo ms")
+}
+
+suspend fun getCapital(): Int {
+    delay(1000L)
+    return 50000
+}
+
+suspend fun getIncome(): Int {
+    delay(1000L)
+    return 75000
+}
